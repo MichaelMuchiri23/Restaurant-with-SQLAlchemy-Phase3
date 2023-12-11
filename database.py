9146dc6551e6ff1c7fc62aa8d92c31ab1e339b77
Michael Muchiri <muchirimichael2222@gmail.com>
2023-12-11 23:52:58 +0300

    Done

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URI = ''
+SQLALCHEMY_DATABASE_URI = '\\\\wsl.localhost\\Ubuntu\\home\\mike\\Development\\code\\Mod3\\code-challenge-3\\Database.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(bind=engine)
